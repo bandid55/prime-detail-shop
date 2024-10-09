fd5d04e069d947e5841de1e247ba476dd88a702b
bandid55 <rbanceo5@gmail.com>
2024-10-08 18:16:20 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,4 +2,3 @@ function toggleMenu() {
     const navList = document.querySelector('nav ul.nav-list');
     navList.classList.toggle('open');
 }
-[_{{{CITATION{{{_1{](https://github.com/yysofiyan/PABWEB-C/tree/cf31bd3408938678b425c6993cf0956da7c4a380/A2.1800125%2Fsiswa%2Findex.php)[_{{{CITATION{{{_2{](https://github.com/Lembrun/blog-system/tree/dd2ef55eb1cd9712a02e6514e5ad2c112cc3fdd3/views%2Flayouts%2Flayout.php)
